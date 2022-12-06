d1d685e4cce19066ea4bf7c68a87562b0cec6a43
Dahlia <rlally89@gmail.com>
2022-12-06 13:58:26 -0800

    storage routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userController');
+const storageRoutes = require('./storageController');
 
 router.use('/users', userRoutes);
+router.use('/storages', storageRoutes);
 
 module.exports = router;
--- controllers/api/storageController.js
+++ controllers/api/storageController.js
@@ -1,9 +1,10 @@
-const router = require('express').Router();
 const express = require('express');
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-const {User,Kitchen, Storage, Product} = require('../../models');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {User, Storage, ShoppingList, Product, Kitchen, DonationList} = require('../../models');
 
+//get all storage types
 router.get('/', async (req, res) => {
     try {
       const storageData = await Storage.findAll();
@@ -12,5 +13,61 @@ router.get('/', async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+//get storage type by id
+router.get('/:id', async (req, res) => {
+    try {
+      const storageData = await Storage.findByPk(req.params.id);
+      if (!storageData) {
+      res.status(404).json({ message: 'No Storage Location found with that ID!' });
+      return;
+      } 
+      res.status(200).json(storageData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+//create storage type
+router.post('/', async (req, res) => {
+  try {
+    const storageData = await Storage.create({
+        storageType: req.body.storageType,
+    });
+    res.status(200).json(storageData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//update storage type
+router.put('/:id', async (req, res) => {
+    try {
+        const storageData = await Storage.update({
+            storageType: req.body.storageType
+        },{
+            where: {
+            id: req.params.id,
+        }
+        });
+        res.status(200).json(storageData)
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const storageData = await Storage.destroy({
+            where: {
+                id: req.params.id
+            },
+        });
+        res.status(200).json(storageData)
+    } catch (err) {
+        res.status(400).json(err);
+    }
+  });
+  
 
 module.exports = router;
