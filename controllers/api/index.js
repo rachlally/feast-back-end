167b5bb60844630d44fa47cda430869cc597c570
Sullisters <heatonpschreiber@gmail.com>
2022-12-06 17:10:18 -0500

    WIP product routing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userController');
+const productRoutes = require('./productController');
 
 router.use('/users', userRoutes);
+router.use('/products', productRoutes);
 
 module.exports = router;
