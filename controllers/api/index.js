2b3a09a902ceff4abcbf554be11a5da014c03371
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2022-12-06 13:56:39 -0800

    kitchen routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userController');
+const kitchenRoutes = require('./kitchenController');
 
 router.use('/users', userRoutes);
+router.use('/kitchens', kitchenRoutes);
 
 module.exports = router;
