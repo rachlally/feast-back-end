9e5d64e3eaac9f798b66ded9264533bc286d814c
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2022-12-06 16:09:56 -0800

    shopping list routes working

--- controllers/api/shoppingController.js
+++ controllers/api/shoppingController.js
@@ -1,20 +1,66 @@
-// const express = require('express');
-// const router = express.Router();
-// const {User, Kitchen, Storage, Product, DonationList, ShoppingList} = require('../../models');
-// const jwt = require('jsonwebtoken');
+const express = require('express');
+const router = express.Router();
+const {User, Kitchen, Storage, Product, DonationList, ShoppingList} = require('../../models');
+const jwt = require('jsonwebtoken');
 
 
-// //get shoppingList
+//get shoppingList
 
-// router.get('/', (req,res)=>{
-//     ShoppingList.findAll({
-//         include:[User]
-//     }).then(shoppingData=>{
-//         res.json(shoppingData)
-//     }).catch(err=>{
-//         console.log(err);
-//         res.json({msg:"an error occured",err})
-//     })
-// })
+router.get('/', (req,res)=>{
+    ShoppingList.findAll({
+        include:[User, Product]
+    }).then(shoppingData=>{
+        res.json(shoppingData)
+    }).catch(err=>{
+        console.log(err);
+        res.json({msg:"an error occured",err})
+    })
+})
 
-// module.exports = router;
+//get one
+router.get('/:id', async (req, res) => {
+    try{
+        const shoppingList = await ShoppingList.findByPk(req.params.id, {
+            include:[User, Product]
+        })
+        res.status(200).json(shoppingList)
+    } catch (err) {
+        console.log(err)
+        res.status(400).json(err)
+    }
+})
+
+//Create shopping list
+router.post('/', async (req, res) => {
+    try{
+      const shoppingList = await ShoppingList.create(req.body);
+      res.status(200).json(shoppingList)
+    } catch (err) {
+      console.log(err)
+      res.status(400).json(err);
+    }
+  });
+
+  //Update List
+  router.put('/:id', async (req, res) => {
+    try{
+      const shoppingList = await ShoppingList.update(req.body,{where:{id:req.params.id}});
+      res.status(200).json(shoppingList)
+    } catch (err) {
+      console.log(err)
+      res.status(400).json(err);
+    }
+  });
+
+  //delete list
+  router.delete('/:id', async(req, res) => {
+    // delete a category by its `id` value
+    try{
+      const shoppingList = await ShoppingList.destroy({where:{id:req.params.id}});
+      res.status(200).json(shoppingList)
+    } catch (err) {
+      console.log(err)
+    }
+  });
+
+module.exports = router;
