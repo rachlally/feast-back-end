2a48a2b2b5f386915be90495be5dd7b5bb2ec15b
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2023-03-11 19:22:29 -0800

    deletion

--- controllers/api/foodBankController.js
+++ controllers/api/foodBankController.js
@@ -19,18 +19,7 @@ router.get("/", async (req, res) => {
     const response = await axios.get(
       `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.query.lat}%2C${req.query.lng}&radius=${req.query.radius}&keyword=food%20bank&key=AIzaSyBVq588qSxiAVHeDMayN1kY-qnHdVMF6CQ`
       )
-      console.log(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.query.lat}%2C${req.query.lng}&radius=${req.query.radius}&keyword=food%20bank&key=AIzaSyBVq588qSxiAVHeDMayN1kY-qnHdVMF6CQ`)
     // console.log(response)
     res.json(response.data) 
   }); 
-
-//   router.get("/", async (req, res) => {
-//         console.log("req: " + req)
-//     const response = await axios.get(
-//       `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=47.6062%2C-122.3321&radius=5000&keyword=food%20bank&key=AIzaSyBVq588qSxiAVHeDMayN1kY-qnHdVMF6CQ`
-//     )
-//     console.log(response.data)
-//     res.json(response.data)
-//   });
-
 module.exports = router;
