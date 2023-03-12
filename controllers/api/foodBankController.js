cea24278563afbb3edebc2b0eb624c53f2da625d
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2023-03-11 19:17:54 -0800

    google maps api sending foodbank data to front end!

--- controllers/api/foodBankController.js
+++ controllers/api/foodBankController.js
@@ -12,12 +12,15 @@ const {
 const axios = require('axios');
 
 router.get("/", async (req, res) => {
-    console.log("req: " + req)
+    // console.log(req)
+    // console.log(req.query.lat)
+    // console.log(req.query.lng)
+    // console.log(req.query.radius)
     const response = await axios.get(
       `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.query.lat}%2C${req.query.lng}&radius=${req.query.radius}&keyword=food%20bank&key=AIzaSyBVq588qSxiAVHeDMayN1kY-qnHdVMF6CQ`
       )
-      console.log(req.query.lat)
-    // console.log(response.data)
+      console.log(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.query.lat}%2C${req.query.lng}&radius=${req.query.radius}&keyword=food%20bank&key=AIzaSyBVq588qSxiAVHeDMayN1kY-qnHdVMF6CQ`)
+    // console.log(response)
     res.json(response.data) 
   }); 
 
